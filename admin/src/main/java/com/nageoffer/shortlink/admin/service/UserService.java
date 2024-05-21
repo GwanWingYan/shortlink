4e76fa2093743301ddbfcd8063bc435a89ef1367
GwanWingYan <47978664+GwanWingYan@users.noreply.github.com>
2024-05-21 22:45:22 +0800

    2.8 检查用户名是否存在功能

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/UserService.java
@@ -14,4 +14,12 @@ public interface UserService extends IService<UserDO> {
      * @return 用户返回实体
      */
     UserRespDTO getUserByUsername(String username);
+
+    /**
+     * 查询用户名是否存在
+     *
+     * @param username 用户名
+     * @return 用户名存在返回 true，不存在返回 false
+     */
+    Boolean hasUsername(String username);
 }

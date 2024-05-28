b97347c2225d81c78dd63559ba2a5d5a85bcaf23
GwanWingYan <47978664+GwanWingYan@users.noreply.github.com>
2024-05-28 19:27:08 +0800

    9.7 监控保存Gid错误&短链唯一

--- database/sql/t_link_goto.sql
+++ database/sql/t_link_goto.sql
@@ -9,114 +9,146 @@
 
 DROP TABLE IF EXISTS `t_link_goto_0`;
 CREATE TABLE `t_link_goto_0` (
-  `id` bigint(20) DEFAULT NULL COMMENT 'ID',
-  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
-  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识'
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid`            varchar(32)  DEFAULT 'default' COMMENT '分组标识',
+    `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_full_short_url` (`full_short_url`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `t_link_goto_1`;
 CREATE TABLE `t_link_goto_1` (
-  `id` bigint(20) DEFAULT NULL COMMENT 'ID',
-  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
-  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识'
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid`            varchar(32)  DEFAULT 'default' COMMENT '分组标识',
+    `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_full_short_url` (`full_short_url`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `t_link_goto_10`;
 CREATE TABLE `t_link_goto_10` (
-  `id` bigint(20) DEFAULT NULL COMMENT 'ID',
-  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
-  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识'
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid`            varchar(32)  DEFAULT 'default' COMMENT '分组标识',
+    `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_full_short_url` (`full_short_url`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `t_link_goto_11`;
 CREATE TABLE `t_link_goto_11` (
-  `id` bigint(20) DEFAULT NULL COMMENT 'ID',
-  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
-  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识'
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid`            varchar(32)  DEFAULT 'default' COMMENT '分组标识',
+    `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_full_short_url` (`full_short_url`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `t_link_goto_12`;
 CREATE TABLE `t_link_goto_12` (
-  `id` bigint(20) DEFAULT NULL COMMENT 'ID',
-  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
-  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识'
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid`            varchar(32)  DEFAULT 'default' COMMENT '分组标识',
+    `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_full_short_url` (`full_short_url`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `t_link_goto_13`;
 CREATE TABLE `t_link_goto_13` (
-  `id` bigint(20) DEFAULT NULL COMMENT 'ID',
-  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
-  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识'
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid`            varchar(32)  DEFAULT 'default' COMMENT '分组标识',
+    `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_full_short_url` (`full_short_url`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `t_link_goto_14`;
 CREATE TABLE `t_link_goto_14` (
-  `id` bigint(20) DEFAULT NULL COMMENT 'ID',
-  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
-  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识'
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid`            varchar(32)  DEFAULT 'default' COMMENT '分组标识',
+    `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_full_short_url` (`full_short_url`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `t_link_goto_15`;
 CREATE TABLE `t_link_goto_15` (
-  `id` bigint(20) DEFAULT NULL COMMENT 'ID',
-  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
-  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识'
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid`            varchar(32)  DEFAULT 'default' COMMENT '分组标识',
+    `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_full_short_url` (`full_short_url`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `t_link_goto_2`;
 CREATE TABLE `t_link_goto_2` (
-  `id` bigint(20) DEFAULT NULL COMMENT 'ID',
-  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
-  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识'
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid`            varchar(32)  DEFAULT 'default' COMMENT '分组标识',
+    `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_full_short_url` (`full_short_url`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `t_link_goto_3`;
 CREATE TABLE `t_link_goto_3` (
-  `id` bigint(20) DEFAULT NULL COMMENT 'ID',
-  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
-  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识'
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid`            varchar(32)  DEFAULT 'default' COMMENT '分组标识',
+    `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_full_short_url` (`full_short_url`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `t_link_goto_4`;
 CREATE TABLE `t_link_goto_4` (
-  `id` bigint(20) DEFAULT NULL COMMENT 'ID',
-  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
-  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识'
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid`            varchar(32)  DEFAULT 'default' COMMENT '分组标识',
+    `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_full_short_url` (`full_short_url`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `t_link_goto_5`;
 CREATE TABLE `t_link_goto_5` (
-  `id` bigint(20) DEFAULT NULL COMMENT 'ID',
-  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
-  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识'
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid`            varchar(32)  DEFAULT 'default' COMMENT '分组标识',
+    `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_full_short_url` (`full_short_url`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `t_link_goto_6`;
 CREATE TABLE `t_link_goto_6` (
-  `id` bigint(20) DEFAULT NULL COMMENT 'ID',
-  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
-  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识'
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid`            varchar(32)  DEFAULT 'default' COMMENT '分组标识',
+    `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_full_short_url` (`full_short_url`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `t_link_goto_7`;
 CREATE TABLE `t_link_goto_7` (
-  `id` bigint(20) DEFAULT NULL COMMENT 'ID',
-  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
-  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识'
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid`            varchar(32)  DEFAULT 'default' COMMENT '分组标识',
+    `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_full_short_url` (`full_short_url`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `t_link_goto_8`;
 CREATE TABLE `t_link_goto_8` (
-  `id` bigint(20) DEFAULT NULL COMMENT 'ID',
-  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
-  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识'
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid`            varchar(32)  DEFAULT 'default' COMMENT '分组标识',
+    `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_full_short_url` (`full_short_url`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 DROP TABLE IF EXISTS `t_link_goto_9`;
 CREATE TABLE `t_link_goto_9` (
-  `id` bigint(20) DEFAULT NULL COMMENT 'ID',
-  `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
-  `gid` varchar(32) DEFAULT 'default' COMMENT '分组标识'
+    `id`             bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `gid`            varchar(32)  DEFAULT 'default' COMMENT '分组标识',
+    `full_short_url` varchar(128) DEFAULT NULL COMMENT '完整短链接',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_full_short_url` (`full_short_url`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 

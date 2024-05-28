2d6529e655225293bf238f802d17dd2cd855c5bf
GwanWingYan <47978664+GwanWingYan@users.noreply.github.com>
2024-05-28 16:10:31 +0800

    8.6 and 8.7 开发短链接聚合服务

--- project/src/main/java/com/nageoffer/shortlink/project/config/DatabaseConfiguration.java
+++ project/src/main/java/com/nageoffer/shortlink/project/config/DatabaseConfiguration.java
@@ -3,9 +3,13 @@ package com.nageoffer.shortlink.project.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 数据库持久层配置类
+ */
 @Configuration
 public class DatabaseConfiguration {
 
@@ -13,6 +17,7 @@ public class DatabaseConfiguration {
      * 分页插件
      */
     @Bean
+    @ConditionalOnBean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
